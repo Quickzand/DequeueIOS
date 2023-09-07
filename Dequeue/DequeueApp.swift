e28a8b6e2258dd52925ce6680097f415af7d41a0
Matthew Sand <44583344+Quickzand@users.noreply.github.com>
2023-09-06 21:46:49 -0400

    Initial Commit

---
+++ Dequeue/DequeueApp.swift
@@ -0,0 +1,17 @@
+//
+//  DequeueApp.swift
+//  Dequeue
+//
+//  Created by Matthew Sand on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct DequeueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
